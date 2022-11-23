35f2a055a9496267c3c8f54246fc7edebb361a95
ok-onkarkhatode <118878794+ok-onkarkhatode@users.noreply.github.com>
2022-11-23 22:18:51 +0530

    Add files via upload

--- Source.c
+++ Source.c
@@ -1,288 +1,109 @@
-/*
-#include<stdio.h>
-#include<stdlib.h>
-#include<conio.h>
-#define MAX 10
-
-struct QUEUE
-{
-  int a[MAX];
-  int front;
-  int rear;
-};
-
-int isfull(struct QUEUE* Qptr)
-{
-	return Qptr->rear ==(MAX -1);
-}
-
- void insert(struct QUEUE* Qptr,int elements)
-{
-	(Qptr->rear)++;
-	Qptr->a[Qptr->rear] = elements;
-}
-
-int isempty(struct QUEUE* Qptr)
-{
-  return Qptr->front == Qptr ->rear;
-}
-
-void del(struct QUEUE* Qptr)
-{
-  (Qptr->front)++;
-  printf("delete element is %d\n",Qptr->a[Qptr->front]);
-}
-
-void display(struct QUEUE* Qptr)
-{
-	int i;
-	//(Qptr->front)++;
-	for(i=Qptr ->front+1; i <=(Qptr->rear);i++)
-	{
-	  printf("elements %d =>",Qptr ->a[i]);
-	}
-	printf("\n");
-}
-
-void main()
-{
-	int choice;
-  int elements;
-  struct QUEUE Q;  //object
-
-  Q.front=Q.rear=-1; //set 
-
-
-  do
-  {
-    printf("1.insert\n");
-	printf("2.delete\n");
-	printf("3.display\n");
-	printf("0.exit\n");
-
-	printf("enter your choice\n");
-	scanf_s("%d",&choice);
-
-	switch(choice)
-	{
-	case 1:
-		if(isfull(&Q))
-		{
-		 printf("Queue is full\n");
-		}
-		else
-		{
-		 
-			printf("enter the elements\n");
-			scanf_s("%d",&elements);
-			insert(&Q,elements);
-		}
-		break;
-	case 2:
-		if(isempty(&Q))
-		{
-		printf("queue is empty \n");
-		}
-		else
-		{
-		 del(&Q);
-		}
-		break;
-	case 3:
-		   if(isempty(&Q))
-		   {
-		     printf("is empty\n");
-		   }
-		   else
-		   {
-		    display(&Q);
-		   }
-		   break;
-	case 0:exit(0);
-		break;
-	}
-  }while(choice != 0);
-  _getch();
-}
-*/
-
-/*
-//circular queue
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
 
-#define MAX 10
-
-struct CQUEUE
+struct node
 {
-  int a[MAX];
-  int front;
-  int rear;
+ int data;
+ struct node* next;
 };
 
-int isfull(struct CQUEUE* CQptr)
+struct node* createnode()
 {
-	return (CQptr->front) ==((CQptr->rear +1)%MAX);
-}
-int insert(struct CQUEUE* CQptr,int elements)
-{
-  CQptr ->rear=(CQptr ->rear+1) % MAX;
-  CQptr ->a[CQptr ->rear] = elements;
-}
+	 int a;
+	 struct node* newnode=NULL;
+	 newnode=(struct node*)malloc(sizeof(struct node));
 
- int isempty(struct CQUEUE* CQptr)
- {
-   return CQptr-> front == CQptr ->rear;
- }
-
- void del(struct CQUEUE* CQptr)
- {
-   CQptr->front = (CQptr->front+1) % MAX;
-  printf("delete element is %d\n",CQptr->a[CQptr->front]);
- }
-
-
- void display(struct QUEUE* CQptr)
-{
-	int i;
-	
-	for(i=(CQptr ->front)+1; i <=(CQptr->rear);i++)
+	if(newnode == NULL)
 	{
-	  printf("elements %d =>",CQptr ->a[i]);
+	   printf("memory not allocated");
 	}
-	printf("\n");
+	else
+	{
+	   printf("enter the a\n");
+	   scanf_s("%d",&a);
+
+	   newnode->data=a;
+	   newnode->next=NULL;
+	}
+	return newnode;
+}
+
+void createLinkedList(struct node** head)
+{
+ struct node* tempnode=NULL;
+ struct node* travnode=*head;
+ tempnode=createnode();
+ if(* head == NULL)
+ {
+  * head = tempnode; 
+ }
+ else
+ {
+   while(travnode->next != NULL)
+   {
+     travnode=travnode->next;
+   }
+   travnode->next=tempnode;
+ }
+}
+
+void displayLinkedList(struct node* head)
+{
+  if(head == NULL)
+  {
+    printf("Linked List is Empty\n");
+	//return NULL;
+  }
+  else
+  {
+    while( head != NULL)
+	{
+	 printf("%d=> ",head->data);
+	 head=head->next;
+	}
+     printf("\n");
+  }
+}
+
+//fibonacci series,factorial,1 to n addition
+void ReverseLinkedList(struct node* head)
+{
+  if(head == NULL)
+  {
+    return;
+  }
+  else
+  {
+    ReverseLinkedList(head -> next);
+	printf("%d =>",head ->data);
+  }
 }
 void main()
 {
 	int choice;
-	int elements;
+	struct node* first = NULL;
+	do
+	{
+	printf("1.create Linked List\n");
+	printf("2.Display Linked List\n");
+	printf("3.reverse linked list\n");
+	printf("0.exit(0)\n");
 
-  struct CQUEUE CQ;  //create object
-   CQ.front= CQ.rear=-1; //set 
-
-  do
-  {
-    printf("1.insert\n");
-	printf("2.delete\n");
-	printf("3.display\n");
-	printf("0.exit\n");
-
-	printf("enter your choice\n");
+	printf("please enter your choice\n");
 	scanf_s("%d",&choice);
 
 	switch(choice)
 	{
-	case 1:
-		if(isfull(&CQ))
-		{
-		 printf("Queue is full\n");
-		}
-		else
-		{
-		 
-			printf("enter the elements\n");
-			scanf_s("%d",&elements);
-			insert(&CQ,elements);
-		}
+      case 1:createLinkedList(&first);
 		break;
-	case 2:
-			if(isempty(&CQ))
-		{
-		printf("queue is empty \n");
-		}
-		else
-		{
-		 del(&CQ);
-		}
-		break;
-	case 3:
-		   if(isempty(&CQ))
-		   {
-		     printf("is empty\n");
-		   }
-		   else
-		   {
-		    display(&CQ);
-		   }
-		   break;
-	case 0:exit(0);
-		break;
+	  case 2:displayLinkedList(first);
+		  break; 
+	  case 3:ReverseLinkedList(first);
+		  break;
+	  case 0:exit(0);
+		  break;
 	}
-  }while(choice != 0);
-  _getch();
+ }while(choice != 0);
+	_getch();
 }
-*/
-
-
-	// single queue
-#include<stdio.h>
-#include<stdlib.h>
-
-	struct queue
-	{
-	 int data;
-	 struct queue* next;
-	
-	};
-
-	struct queue* createqueue()
-	{
-		struct queue* newnode=NULL;
-	  newnode=(struct queue*)malloc(sizeof(struct queue));
-	  if(newnode == NULL)
-	  {
-	    printf("momery is not allocated");
-		return;
-	  }
-	  else
-	  {
-		  int a;
-	    printf("please enter the data\n");
-		scanf("%d",a);
-		newnode->data=a;
-		newnode->next = NULL;
-		return newnode;
-	  }
-	}
-	
-	 void insert(struct queue** frontptr ,struct queue** raerptr)
-	 {
-	 struct queue* tempnode = NULL;
-	 tempnode = createqueue();
-	 
-	 }
-	void main()
-	{
-		int choice,elements;
-		struct queue* front = NULL;
-		struct queue* rear = NULL;
-	 
-		do
-		{
-		  printf("1. insert\n");
-		  printf("2. delete\n");
-		  printf("3. display\n");
-		  printf("0. exit\n");
-
-		  printf("enter your choice\n");
-		  scanf("%d",&choice);
-
-		  switch(choice)
-		  {
-		  case 1:printf("eneter the elements\n");
-			    scanf("%d",elements);
-				if(full())
-				{
-				  printf("queue is full\n");
-				}
-				else
-				{
-				 insert(front , rear);
-				}
-		  }
-		}
-	
-	}
-
